ee0387d74b65982f36939a117787fccb67341be4
Markus Veem <markusv@tlu.ee>
2022-11-05 18:27:18 +0200

    new_branch and removing unneccesary files

--- project/functions/helloWorld.js
+++ project/functions/helloWorld.js
@@ -1,6 +1,6 @@
 exports.handler = async (event, context) => {
-    return {
-        statusCode: 200,
-        body: JSON.stringify({ message: "Hello world.." }),
-    };
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: "Hello World.." }),
+  };
 };
