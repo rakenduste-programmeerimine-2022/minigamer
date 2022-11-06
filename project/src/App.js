4ef9b86c791c2015f815e7827637c7bf57cb1612
Markus Veem <markusv@tlu.ee>
2022-11-06 17:11:22 +0200

    mui demo close #32

--- project/src/App.js
+++ project/src/App.js
@@ -1,16 +1,19 @@
 import logo from "./logo.svg";
 import "./App.css";
 import SetupExample from "./components/SetupExample";
+import Demo from "./components/Demo";
 
 function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <SetupExample />
-            </header>
-        </div>
-    );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>AAAAA</p>
+        <img src={logo} className="App-logo" alt="logo" />
+        <SetupExample />
+        <Demo />
+      </header>
+    </div>
+  );
 }
 
 export default App;
