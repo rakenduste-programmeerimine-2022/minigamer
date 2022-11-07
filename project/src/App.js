d5c6ea1b21583ab43ed3423265627c5c1eb61b54
Markus Veem <markusv@tlu.ee>
2022-11-07 02:11:39 +0200

    basic react routing closes #6

--- project/src/App.js
+++ project/src/App.js
@@ -2,17 +2,38 @@ import logo from "./logo.svg";
 import "./App.css";
 import SetupExample from "./components/SetupExample";
 import Demo from "./components/Demo";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  RouterProvider,
+  createBrowserRouter,
+} from "react-router-dom";
+import Leaderboard from "./pages/Leaderboard";
+import Games from "./pages/Games";
+import Profile from "./pages/Profile";
+import Home from "./pages/Home";
+import ErrorPage from "./pages/ErrorPage";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>AAAAA</p>
-        <img src={logo} className="App-logo" alt="logo" />
-        <SetupExample />
-        <Demo />
-      </header>
-    </div>
+    <Router>
+      <Header />
+      <div className="App">
+        <header className="App-header">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="profile/:username" element={<Profile />} />
+            <Route path="games" element={<Games />} />
+            <Route path="leaderboard" element={<Leaderboard />} />
+            <Route path="*" element={<ErrorPage />} />
+          </Routes>
+        </header>
+      </div>
+      <div>footer</div>
+    </Router>
   );
 }
 
---
+++ project/src/pages/Games.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Games() {
+  return <div>games</div>;
+}
+
+export default Games;
