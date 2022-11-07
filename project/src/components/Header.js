0dfaae7f661a6efca077b4de90398f4bd6798212
Markus Veem <markusv@tlu.ee>
2022-11-07 12:42:09 +0200

    removed unused imports

--- project/src/components/Header.js
+++ project/src/components/Header.js
@@ -1,11 +1,5 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link,
-  RouterProvider,
-} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
