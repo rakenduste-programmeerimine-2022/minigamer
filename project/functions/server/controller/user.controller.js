c3cd49f4f914b9d417ee8d35f828efa1b89d752a
Taavi Pahk <taavipahk43@gmail.com>
2022-11-09 00:31:53 +0200

    close #15, #16, #22, #35, #36 (reusing code ftw)

--- project/functions/server/controller/user.controller.js
+++ project/functions/server/controller/user.controller.js
@@ -1,30 +1,52 @@
-// const jwt = require("jsonwebtoken");
-// const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 require("dotenv").config();
 
-const saltRounds = 10;
+const User = require("../model/user.model");
+const { messageJSON, errorJSON } = require("../../utility/log");
+
+const ROUNDS = 10;
 
 exports.anyRequest = (req, res, next) => {
-    console.log(`${req.method} USER REQUEST AT ${Date.now()}`);
+    let { username, password } = req.body;
+    if (username == null || password == null) {
+        return res.send(messageJSON("Missing username and/or password."));
+    }
+    req.body.username = username.toString();
+    req.body.password = password.toString();
     next();
 };
 
 exports.register = async (req, res) => {
-    const name = req.body.name;
-    const pass = req.body.pass;
-    let hashPass;
-    // bcrypt.hash(pass, saltRounds, async (err, hash) => {
-    //     if (err) {
-    //         console.log(err);
-    //         res.end();
-    //     }
-    //     hashPass = hash;
-    // });
-    res.send({ name, pass, state: "Registered" });
+    const { username, password } = req.body;
+    // unique field in schema should already prevent duplicates?
+    if (await User.exists({ username })) {
+        return res.send(messageJSON("User already exists."));
+    }
+    let user;
+    bcrypt.hash(password, ROUNDS, async (error, hash) => {
+        if (error) {
+            return res.send(errorJSON(error));
+        }
+        user = await User.create({ username, password: hash });
+    });
+    return res.send(messageJSON("Account registration successful!"));
 };
 
 exports.login = async (req, res) => {
-    const name = req.body.name;
-    const pass = req.body.pass;
-    res.send({ name, state: "Logged in" });
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    if (!user) {
+        return res.send(messageJSON("User does not exist."));
+    }
+    bcrypt.compare(password, user.password, (error, result) => {
+        if (error) {
+            return res.send(errorJSON(error));
+        }
+        if (!result) {
+            return res.send(messageJSON("Wrong password."));
+        }
+        const token = jwt.sign({ username }, process.env.JWT_SECRET_KEY);
+        return res.send({ token });
+    });
 };
