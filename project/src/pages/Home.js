34794794338e86fcaac63d12f7dc0ea6cd03295b
Markus Veem <markusv@tlu.ee>
2022-11-12 12:14:21 +0200

    fixed naming

--- project/src/pages/Home.js
+++ project/src/pages/Home.js
@@ -2,16 +2,16 @@ import React from "react";
 import Box from "@mui/material/Box";
 import HeroImage from "../components/HeroImage";
 import GamesSlider from "../components/GamesSlider";
-import LeaderBoards_Section from "../components/LeaderBoardsSection";
-import DailyChallenge_section from "../components/DailyChallengeSection";
+import LeaderBoardsSection from "../components/LeaderBoardsSection";
+import DailyChallengeSection from "../components/DailyChallengeSection";
 
 function Home() {
   return (
     <Box>
       <HeroImage></HeroImage>
       <GamesSlider></GamesSlider>
-      <LeaderBoards_Section></LeaderBoards_Section>
-      <DailyChallenge_section></DailyChallenge_section>
+      <LeaderBoardsSection></LeaderBoardsSection>
+      <DailyChallengeSection></DailyChallengeSection>
     </Box>
   );
 }
