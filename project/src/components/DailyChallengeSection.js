3c171f05b8bb623942f77d9b21e4d02857bc97cf
Markus Veem <markusv@tlu.ee>
2022-11-13 16:42:00 +0200

    work on home page

--- project/src/components/DailyChallengeSection.js
+++ project/src/components/DailyChallengeSection.js
@@ -1,24 +1,50 @@
 import React from "react";
 import { Box } from "@mui/system";
-import { Typography } from "@mui/material";
+import { Container, Typography } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
-function DailyChallenge_section() {
+function DailyChallengeSection() {
+  let navigate = useNavigate();
+
   return (
-    <Box className="dailyChallenge_section">
-      <Typography>Complete Daily challenges</Typography>
-      <Box sx={{ width: 1, height: 400, backgroundColor: "black" }}>
-        <Typography
-          sx={{
-            color: "white",
-            display: "flex",
-            justifyContent: "center",
+    <Box
+      className="dailyChallengeSection"
+      sx={{ width: 1, height: 1, backgroundColor: "black" }}
+    >
+      <Container className="dailyChallengeWrapper">
+        <Box
+          className="leftDiv"
+          onClick={() => {
+            navigate("/leaderboard");
           }}
         >
-          DailyChallenge
-        </Typography>
-      </Box>
+          {/* <Box
+            className="challengeImg"
+            sx={{ backgroundColor: "black", width: 1, height: 1 }}
+          ></Box> */}
+          <Box
+            className="dailyChallenge"
+            sx={{ backgroundColor: "white", width: 1, height: 1 }}
+          >
+            {" "}
+            <Typography className="dailyText" variant="h5">
+              Daily Challenge
+            </Typography>
+          </Box>
+        </Box>
+        <Box className="rightDiv">
+          <Typography className="title">Complete Daily challenges</Typography>
+          <Box>
+            <Typography className="text">
+              Everyday there is a special challenge for one randomly selected
+              game. All players can play game with the same seed and compete
+              with each other.
+            </Typography>
+          </Box>
+        </Box>
+      </Container>
     </Box>
   );
 }
 
-export default DailyChallenge_section;
+export default DailyChallengeSection;
