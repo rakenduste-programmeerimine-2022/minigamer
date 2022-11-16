ca57bbd3aeb5b4cc09caa8e7a4b38f99cd80c088
Markus Veem <markusv@tlu.ee>
2022-11-16 23:22:55 +0200

    added some test imges and imported new font

--- project/src/components/GamesSliderData.js
+++ project/src/components/GamesSliderData.js
@@ -1,20 +1,24 @@
+import MineSweeperimage from "../Images/minesweeper.jpg";
+import Sudokuimage from "../Images/Sudoku.png";
+import Nonogrammimage from "../Images/nongramm.jpg";
+
 export const GamesSliderData = [
   {
     link: "/games/Minesweeper",
     name: "Minesweeper",
-    image: "../../public/images/Minesweeper_9x9_10_example_16.png",
+    image: MineSweeperimage,
     desc: "Minesweeper is a logic puzzle video game genre generally played on personal computers. The game features a grid of clickable squares, with hidden 'mines' scattered throughout the board. The objective is to clear the board without detonating any mines, with help from clues about the number of neighboring mines in each field",
   },
   {
     link: "/games/Sudoku",
     name: "Sudoku",
-    image: "",
+    image: Sudokuimage,
     desc: "Sudoku originally called Number Place)[1] is a logic-based,[2][3] combinatorial[4] number-placement puzzle. In classic Sudoku, the objective is to fill a 9 × 9 grid with digits so that each column, each row, and each of the nine 3 × 3 subgrids that compose the grid (also called boxes, blocks, or regions) contain all of the.",
   },
   {
     link: "/games/Nonogramm",
     name: "Nonogramm",
-    image: "",
+    image: Nonogrammimage,
     desc: "Nonograms, also known as Hanjie, Paint by Numbers,Picross, Griddlers, and Pic-a-Pix, and by various other names, are picture logic puzzles in which cells in a grid must be colored or left blank according to numbers at the side of the grid to reveal a hidden pixel art-like picture. In this puzzle type, the numbers are a form .",
   },
 ];
