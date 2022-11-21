8533cf93de77dfe383bf91788d2dd1fcc3291808
Taavi Pahk <95106680+taavippp@users.noreply.github.com>
2022-11-22 00:47:36 +0200

    close #76, could use visual improvements

--- project/src/pages/Nonogram.js
+++ project/src/pages/Nonogram.js
@@ -1,4 +1,4 @@
-import { Button } from "@mui/material";
+import { Button, Skeleton, Typography } from "@mui/material";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React, { useState } from "react";
 
@@ -16,28 +16,63 @@ const queryClient = new QueryClient({
 });
 
 function Nonogram() {
-    const [showGame, setShowGame] = useState(false);
-    const [sessionID, setSessionID] = useState(0);
+    const [state, setState] = useState({
+        showGame: false,
+        gameID: 0,
+        gameWon: false,
+    });
 
-    const onClick = () => {
-        setSessionID(sessionID + 1);
-        if (showGame) {
+    const newGame = () => {
+        setState({
+            showGame: true,
+            gameID: state.gameID + 1,
+            gameWon: false,
+        });
+        if (state.showGame) {
             queryClient.refetchQueries();
             return;
         }
-        setShowGame(true);
+    };
+
+    const setGameWon = (value) => {
+        if (value === state.gameWon) {
+            return;
+        }
+        setState({
+            showGame: state.showGame,
+            gameID: state.gameID,
+            gameWon: value,
+        });
+    };
+
+    const submitScore = () => {
+        if (!state.gameWon) {
+        }
     };
 
     return (
         <QueryClientProvider client={queryClient}>
-            {showGame ? (
+            <Typography variant="h2">Nonogram</Typography>
+            <Button onClick={newGame}>New game</Button>
+            {state.showGame ? (
                 <>
-                    <Button onClick={onClick}>New game</Button>
-                    <Game key={sessionID} />
+                    <Button onClick={submitScore} disabled={!state.gameWon}>
+                        Submit score
+                    </Button>
+                    <Game
+                        key={state.gameID}
+                        setGameWon={setGameWon}
+                        className={`${state.gameWon ? "BoardSolved" : ""}`}
+                    />
                 </>
             ) : (
                 <>
-                    <Button onClick={onClick}>New game</Button>
+                    <Skeleton
+                        variant="rectangular"
+                        height={500}
+                        width={500}
+                        animation={false}
+                    />
                 </>
             )}
         </QueryClientProvider>
