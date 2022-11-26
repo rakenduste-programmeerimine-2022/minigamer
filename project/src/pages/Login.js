3beb4551a3e82f81cba278283c84a4ec37455039
Markus Veem <markusv@tlu.ee>
2022-11-26 18:54:37 +0200

    displaying error messages

--- project/src/pages/Login.js
+++ project/src/pages/Login.js
@@ -13,55 +13,57 @@ const Login = (props) => {
     e.preventDefault();
     console.log(username);
     //setSuccess(true);
+    const LoginURL = "../.netlify/functions/server/user/login";
 
     axios
-      .post("http://localhost:8888/.netlify/functions/server/user/login", {
+      .post(LoginURL, {
         username: username,
         password: password,
       })
       .then((res) => {
-        setLoginStatus("logged in");
+        setLoginStatus(res.data.text);
+      })
+      .catch((err) => {
+        console.log(err);
+        setLoginStatus(err.response.data.text);
       });
   };
 
   return (
     <div className="authContainer">
       <h1>Login</h1>
-      {loginStatus ? (
-        <h1>you are logged in</h1>
-      ) : (
-        <form className="authForm" onSubmit={handleSubmit}>
-          <label className="labels" htmlFor="username">
-            username
-            <input
-              className="username"
-              placeholder="Username"
-              id="username"
-              name="username"
-              ref={userRef}
-              onChange={(e) => setusername(e.target.value)}
-              value={username}
-              required
-            />
-          </label>
+      <form className="authForm" onSubmit={handleSubmit}>
+        <label className="labels" htmlFor="username">
+          username
+          <input
+            className="username"
+            placeholder="Username"
+            id="username"
+            name="username"
+            ref={userRef}
+            onChange={(e) => setusername(e.target.value)}
+            value={username}
+            required
+          />
+        </label>
 
-          <label className="labels" htmlFor="password">
-            password
-            <input
-              className="password"
-              type="password"
-              placeholder="*******"
-              id="password"
-              name="password"
-              onChange={(e) => setPass(e.target.value)}
-              value={password}
-            />
-          </label>
-          <button className="submitBtn btn" type="submit">
-            Login
-          </button>
-        </form>
-      )}
+        <label className="labels" htmlFor="password">
+          password
+          <input
+            className="password"
+            type="password"
+            placeholder="*******"
+            id="password"
+            name="password"
+            onChange={(e) => setPass(e.target.value)}
+            value={password}
+          />
+        </label>
+        <button className="submitBtn btn" type="submit">
+          Login
+        </button>
+        {loginStatus}
+      </form>
     </div>
   );
 };
--- project/src/pages/Register.js
+++ project/src/pages/Register.js
@@ -7,61 +7,63 @@ const Register = (props) => {
 
   const [username, setusername] = useState("");
   const [password, setPass] = useState("");
-  const [success, setSuccess] = useState(false);
+  const [regStatus, setRegStatus] = useState(false);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     console.log(username);
     //setSuccess(true);
+    const RegisterURL = "../.netlify/functions/server/user/register";
 
     axios
-      .post("http://localhost:8888/.netlify/functions/server/user/register", {
+      .post(RegisterURL, {
         username: username,
         password: password,
       })
       .then((res) => {
-        setSuccess("registrated");
+        setRegStatus(res.data.text);
+      })
+      .catch((err) => {
+        setRegStatus(err.response.data.text);
       });
   };
 
   return (
     <div className="authContainer">
       <h1>Register</h1>
-      {success ? (
-        <h1>you are registrated</h1>
-      ) : (
-        <form className="authForm" onSubmit={handleSubmit}>
-          <label className="labels" htmlFor="username">
-            username
-            <input
-              className="username"
-              placeholder="Username"
-              id="username"
-              name="username"
-              ref={userRef}
-              onChange={(e) => setusername(e.target.value)}
-              value={username}
-              required
-            />
-          </label>
+      <form className="authForm" onSubmit={handleSubmit}>
+        <label className="labels" htmlFor="username">
+          username
+          <input
+            className="username"
+            placeholder="Username"
+            id="username"
+            name="username"
+            ref={userRef}
+            onChange={(e) => setusername(e.target.value)}
+            value={username}
+            required
+          />
+        </label>
 
-          <label className="labels" htmlFor="password">
-            password
-            <input
-              className="password"
-              type="password"
-              placeholder="*******"
-              id="password"
-              name="password"
-              onChange={(e) => setPass(e.target.value)}
-              value={password}
-            />
-          </label>
-          <button className="submitBtn btn" type="submit">
-            Register
-          </button>
-        </form>
-      )}
+        <label className="labels" htmlFor="password">
+          password
+          <input
+            className="password"
+            type="password"
+            placeholder="*******"
+            id="password"
+            name="password"
+            onChange={(e) => setPass(e.target.value)}
+            value={password}
+            required
+          />
+        </label>
+        <button className="submitBtn btn" type="submit">
+          Register
+        </button>
+        {regStatus}
+      </form>
     </div>
   );
 };
