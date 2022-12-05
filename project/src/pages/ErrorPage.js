a226d2e133d865cfd2db41ed0843dedfea6f4d22
Markus Veem <markusv@tlu.ee>
2022-12-05 14:33:23 +0200

    basic design of errorPage close #97

--- project/src/pages/ErrorPage.js
+++ project/src/pages/ErrorPage.js
@@ -1,7 +1,24 @@
 import React from "react";
+import Box from "@mui/material/Box";
+import "../Styles/ErrorPage.scss";
+import { Button, Typography } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 function ErrorPage() {
-  return <div>ErrorPage</div>;
+  let navigate = useNavigate();
+
+  return (
+    <Box className="errorPage" id="errorPage">
+      <Box className="errorWrap">
+        <Typography className="title">
+          Page doesnt exist or has been moved
+        </Typography>
+        <Button onClick={() => navigate("/")} className="btn">
+          Back to home
+        </Button>
+      </Box>
+    </Box>
+  );
 }
 
 export default ErrorPage;
