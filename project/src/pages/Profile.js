ff02c7178b4e442324dcb290b9d0b889dab30ff7
Markus Veem <markusv@tlu.ee>
2022-12-05 16:08:28 +0200

    following pop up

--- project/src/pages/Profile.js
+++ project/src/pages/Profile.js
@@ -1,5 +1,5 @@
-import React, { useContext } from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import React, { useContext, useState } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import "../Styles/Profile.scss";
 import {
   Box,
@@ -7,13 +7,27 @@ import {
   Accordion,
   AccordionSummary,
   AccordionDetails,
+  Button,
+  Modal,
 } from "@mui/material";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import { UserContext } from "../App";
 
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: "80%",
+  height: "50%",
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
+
 function Profile() {
   let navigate = useNavigate();
-
   const user = useContext(UserContext);
   let Currentusername = null;
 
@@ -25,52 +39,136 @@ function Profile() {
   let { username } = useParams();
   let following = ["juss", "suss", "muss", "kuss"];
   let followers = ["juss", "suss", "muss", "kuss"];
+  let isDisabled = false;
+  function closeAccordions() {
+    isDisabled = true;
+  }
+  function followUser(name) {
+    console.log("user followed : ", name);
+  }
+
+  const [open, setOpen] = useState({ windowOpen: false, window: "" });
+  const handleOpen = (list) => setOpen({ windowOpen: true, window: list });
+  const handleClose = () => setOpen({ windowOpen: false, window: "" });
+
   return (
     <Box className="profile" id="profile">
       <Box className="profileWrapper">
-        <Typography className="user">{username} profile</Typography>
+        <Box className="userSection">
+          <Typography className="user">{username} profile</Typography>
+          {username !== Currentusername && (
+            <Button className="btn" onClick={() => followUser(username)}>
+              Follow user
+            </Button>
+          )}
+        </Box>
+        <Box className="divider"></Box>
+        <Box className="followPopUpSection">
+          <Button onClick={() => handleOpen("Followers")} className="btn">
+            Followers
+          </Button>
+          <Button onClick={() => handleOpen("Following")} className="btn">
+            Following
+          </Button>
+          <Modal
+            open={open.windowOpen}
+            onClose={handleClose}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+          >
+            <Box sx={style}>
+              <Typography id="modal-modal-title" variant="h6" component="h2">
+                {open.window}
+              </Typography>
+              {open.window === "Followers" ? (
+                <>
+                  {followers.map((follower) => {
+                    return (
+                      <Link
+                        to={`/profile/${follower}`}
+                        onClick={handleClose}
+                        key={follower}
+                      >
+                        <Typography> {follower}</Typography>
+                      </Link>
+                    );
+                  })}
+                </>
+              ) : (
+                <>
+                  {following.map((followee) => {
+                    return (
+                      <Link
+                        onClick={handleClose}
+                        to={`/profile/${followee}`}
+                        key={followee}
+                      >
+                        <Typography>{followee}</Typography>
+                      </Link>
+                    );
+                  })}
+                </>
+              )}
+            </Box>
+          </Modal>
+        </Box>
+        <Box className="gameDataSection">
+          <Button className="btn" onClick={() => navigate("/leaderboard")}>
+            Scores
+          </Button>
+        </Box>
+        {Currentusername === username && (
+          <Box>
+            Seda näeb ainult enda profiilil kui tahame midagi siukest teha
+          </Box>
+        )}
+        <Box className="followSection">
+          <Box className="following">
+            <Accordion>
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel1a-content"
+                id="panel1a-header"
+              >
+                <Typography>Following</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                {followers.map((follower) => {
+                  return (
+                    <Link to={`/profile/${follower}`} key={follower}>
+                      <Typography> {follower}</Typography>
+                    </Link>
+                  );
+                })}
+              </AccordionDetails>
+            </Accordion>
+          </Box>
+          <Box className="followers">
+            <Accordion disabled={isDisabled}>
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel1a-content"
+                id="panel1a-header"
+              >
+                <Typography>Following</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                {following.map((followee) => {
+                  return (
+                    <Link
+                      onClick={() => closeAccordions()}
+                      to={`/profile/${followee}`}
+                      key={followee}
+                    >
+                      <Typography>{followee}</Typography>
+                    </Link>
+                  );
+                })}
+              </AccordionDetails>
+            </Accordion>
+          </Box>
+        </Box>
       </Box>
-      <Box className="divider"></Box>
-      <Box className="followSection">
-        <Box className="following">
-          <Accordion>
-            <AccordionSummary
-              expandIcon={<ExpandMoreIcon />}
-              aria-controls="panel1a-content"
-              id="panel1a-header"
-            >
-              <Typography>Following</Typography>
-            </AccordionSummary>
-            <AccordionDetails>
-              {followers.map((follower) => {
-                return <Typography key={follower}>{follower}</Typography>;
-              })}
-            </AccordionDetails>
-          </Accordion>
-        </Box>
-        <Box className="followers">
-          <Accordion>
-            <AccordionSummary
-              expandIcon={<ExpandMoreIcon />}
-              aria-controls="panel1a-content"
-              id="panel1a-header"
-            >
-              <Typography>Following</Typography>
-            </AccordionSummary>
-            <AccordionDetails>
-              {following.map((followee) => {
-                return <Typography key={followee}>{followee}</Typography>;
-              })}
-            </AccordionDetails>
-          </Accordion>
-        </Box>
-      </Box>
-      <Box className="gameDataSection"></Box>
-      {Currentusername === username && (
-        <Box>
-          Seda näeb ainult enda profiilil kui tahame midagi siukest teha
-        </Box>
-      )}
     </Box>
   );
 }
