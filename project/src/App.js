2e6263337d848c6e4a7af5501fe5fc6e940e9dea
Markus Veem <markusv@tlu.ee>
2022-12-07 00:33:11 +0200

    some reactQuery work

--- project/src/App.js
+++ project/src/App.js
@@ -9,52 +9,43 @@ import ErrorPage from "./pages/ErrorPage";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Login from "./pages/Login";
-import Minesweeper from "./pages/Minesweeper";
+/* import Minesweeper from "./pages/Minesweeper";
 import Flood from "./pages/Flood";
+import Nonogram from "./pages/Nonogram"; */
 import React, { createContext, useState } from "react";
 import Register from "./pages/Register";
-import Nonogram from "./pages/Nonogram";
 import GamePage from "./pages/GamePage";
 
 export const UserContext = createContext();
 
 function App() {
-    const [currentUser, setCurrentUser] = useState(
-        JSON.parse(window.sessionStorage.getItem("user"))
-    );
+  const [currentUser, setCurrentUser] = useState(
+    JSON.parse(window.sessionStorage.getItem("user"))
+  );
 
-    return (
-        <Router>
-            <UserContext.Provider value={[currentUser, setCurrentUser]}>
-                <Header />
-                <div className="App">
-                    <Routes>
-                        <Route path="/" element={<Home />} />
-                        <Route
-                            path="/profile/:username"
-                            element={<Profile />}
-                        />
-                        <Route path="/games" element={<Games />} />
-                        {/* <Route path="/games/:id" element={<GamePage />} />
+  return (
+    <Router>
+      <UserContext.Provider value={[currentUser, setCurrentUser]}>
+        <Header />
+        <div className="App">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/profile/:username" element={<Profile />} />
+            <Route path="/games" element={<Games />} />
             <Route path="/games/:id" element={<GamePage />} />
-            <Route path="/games/:id" element={<GamePage />} /> */}
-                        <Route path="/games/:id" element={<GamePage />} />
-                        <Route path="/games/nonogram" element={<Nonogram />} />
-                        <Route path="/games/flood" element={<Flood />} />
-                        <Route
-                            path="/games/minesweeper"
-                            element={<Minesweeper />}
-                        />
-                        <Route path="/leaderboard" element={<Leaderboard />} />
-                        <Route path="/login" element={<Login />} />
-                        <Route path="/register" element={<Register />} />
-                        <Route path="*" element={<ErrorPage />} />
-                    </Routes>
-                </div>
-                <Footer />
-            </UserContext.Provider>
-        </Router>
-    );
+            {/* <Route path="/games/nonogram" element={<Nonogram />} />
+            <Route path="/games/flood" element={<Flood />} />
+            <Route path="/games/minesweeper" element={<Minesweeper />} /> */}
+            <Route path="/leaderboard" element={<Leaderboard />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="*" element={<ErrorPage />} />
+          </Routes>
+        </div>
+        <Footer />
+      </UserContext.Provider>
+    </Router>
+  );
 }
 
 export default App;
