e727f6649121c6a203667830212ada0a4e7bce51
Markus Veem <markusv@tlu.ee>
2022-12-15 17:10:27 +0200

    leaderboards pagination

--- project/src/components/Lborad.js
+++ project/src/components/Lborad.js
@@ -18,52 +18,30 @@ import axios from "axios";
 import { DataGrid } from "@mui/x-data-grid";
 
 const Lborad = ({ testscoreData, searchSetter, changeData }) => {
-  //const [page, setPage] = useState(0);
-  const [rowsPerPage, setRowsPerPage] = useState(10);
+  const [rowsPerPage, setRowsPerPage] = useState(25);
+  const [PAGE, setPAGE] = useState(1);
   const [open, setOpen] = useState(false);
   const handleChangePage = (e, newPage) => {
-    setPage(newPage);
-    //console.log("page");
+    console.log(newPage);
+    setPAGE(newPage + 1);
   };
   const handleChangeRowsPerPage = (e) => {
     setRowsPerPage(+e.target.value);
-    setPage(0);
+    setPAGE(0);
   };
 
+  console.log(PAGE);
   const sessionUser = sessionStorage.getItem("user");
   const { username } = JSON.parse(sessionUser);
-  console.log(sessionUser);
   const searchParams = changeData();
   let GAME = searchParams[0].game;
   let DATE = searchParams[0].date;
   let USER = searchParams[0].usersearch;
-  const [PAGE, setPAGE] = useState(1);
+  let callQuery = searchParams[0].callQuery;
+  console.log(callQuery);
   let scores = [];
   console.log(GAME, DATE, USER);
-  /* 
-  // Returns scores of users followed by {USER}.
-  if (USER === "Followed" && GAME !== "Daily") {
-    URL = `../.netlify/functions/server/score/u/${username}/following/${PAGE}?game=${GAME}&date=${DATE}`;
-  }
-  // Returns scores made by {USER}. you
-  if (USER === "You" && GAME !== "Daily") {
-    curUser = username;
-    URL = `../.netlify/functions/server/score/u/${curUser}/${PAGE}?game=${GAME}&date=${DATE}`;
-  }
-  // Returns scores made by {USER}.
-  if (
-    USER !== "You" &&
-    USER !== "Followed" &&
-    USER !== "everyone" &&
-    GAME !== "Daily"
-  ) {
-    curUser = USER;
-    URL = `../.netlify/functions/server/score/u/${curUser}/${PAGE}?game=${GAME}&date=${DATE}`;
-  }
-
-  if (GAME === "All") {
-    URL = `../.netlify/functions/server/score/u/testuser/1?date=2022-12-14`;
-  } */
+  if (!USER) USER = "everyone";
 
   let URL = `../.netlify/functions/server/score/g/nonogram/${PAGE}`;
   // search games by date or all dates
@@ -78,7 +56,7 @@ const Lborad = ({ testscoreData, searchSetter, changeData }) => {
   if (GAME === "Daily") {
     let today = new Date();
     const dd = String(today.getDate()).padStart(2, "0");
-    const mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
+    const mm = String(today.getMonth() + 1).padStart(2, "0");
     const yyyy = today.getFullYear();
     today = yyyy + "-" + mm + "-" + dd;
     console.log(today);
@@ -114,19 +92,14 @@ const Lborad = ({ testscoreData, searchSetter, changeData }) => {
     URL = `../.netlify/functions/server/score/u/${USER}/${PAGE}${gameaddon}${dateaddon}`;
   }
 
-  const [page, setPage] = useState(1);
   const { isLoading, isFetching, error, data } = useQuery(
-    ["Leaderboards", searchParams[0]],
+    ["Leaderboards", PAGE, searchParams[0]],
     async () => {
       const res = await axios.get(URL);
       return res.data;
     }
     /* { initialData: "" } */
   );
-
-  if (!isLoading) {
-    //console.log(data);
-  }
 
   useEffect(() => {
     if (error) {
@@ -138,6 +111,7 @@ const Lborad = ({ testscoreData, searchSetter, changeData }) => {
   if (data) {
     scores = data.object.scores;
     console.log(data);
+    console.log(data.object.scores.length);
   }
 
   return (
@@ -146,42 +120,39 @@ const Lborad = ({ testscoreData, searchSetter, changeData }) => {
         <Table className="boardTable">
           <TableBody>
             <TableRow className="titleRow">
+              <TableCell key={"rank"}>Rank</TableCell>
               <TableCell key={"user"}>User</TableCell>
               <TableCell key={"game"}>Game</TableCell>
-              <TableCell key={"Challenge Type"}>Challenge Type</TableCell>
               <TableCell key={"Score"}>Score</TableCell>
               <TableCell key={"Time"}>Time</TableCell>
             </TableRow>
             {data ? (
-              scores //scores //testscoreData
-                /*.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)*/
-                .map((data, index) => {
-                  return (
-                    <TableRow key={index} className="dataRow">
-                      <TableCell key={data.username + index}>
-                        {data.username}
-                      </TableCell>
-                      <TableCell key={data.gameID + index + "game"}>
-                        {USER !== "everyone"
-                          ? data.gameID === 0
-                            ? "Nonogram"
-                            : data.gameID === 1
-                            ? "Minesweeper"
-                            : "Flood"
-                          : GAME}
-                      </TableCell>
-                      <TableCell
-                        key={
-                          GAME === "Daily" + index ? GAME : "Default" + index
-                        }
-                      >
-                        {GAME === "Daily" ? GAME : "Default"}
-                      </TableCell>
-                      <TableCell key={data.score}>{data.score}</TableCell>
-                      <TableCell key={DATE}>{data.date}</TableCell>
-                    </TableRow>
-                  );
-                })
+              scores.map((data, index) => {
+                return (
+                  <TableRow key={index} className="dataRow">
+                    <TableCell key={(PAGE - 1) * 25 + index + 1}>
+                      {(PAGE - 1) * 25 + index + 1}
+                    </TableCell>
+
+                    <TableCell key={data.username + index}>
+                      {data.username}
+                    </TableCell>
+                    <TableCell key={data.gameID + index + "game"}>
+                      {USER !== "everyone"
+                        ? data.gameID === 0
+                          ? "Nonogram"
+                          : data.gameID === 1
+                          ? "Minesweeper"
+                          : data.gameID === 2
+                          ? "Flood"
+                          : "Nonogram" /* default search is nonogram */
+                        : GAME}
+                    </TableCell>
+                    <TableCell key={data.score}>{data.score}</TableCell>
+                    <TableCell key={DATE}>{data.date?.split("T")[0]}</TableCell>
+                  </TableRow>
+                );
+              })
             ) : (
               <>
                 <TableRow
@@ -193,15 +164,22 @@ const Lborad = ({ testscoreData, searchSetter, changeData }) => {
           </TableBody>
         </Table>
       </TableContainer>
-      {/* <TablePagination
-        rowsPerPageOptions={[2, 5, 10]}
-        component={"div"}
-        count={testscoreData.length}
-        rowsPerPage={rowsPerPage}
-        page={page}
-        onPageChange={handleChangePage}
-        onRowsPerPageChange={handleChangeRowsPerPage}
-      ></TablePagination> */}
+      {data && (
+        <TablePagination
+          rowsPerPageOptions={[rowsPerPage]}
+          component={"div"}
+          count={
+            data.object.scores.length === rowsPerPage
+              ? -1
+              : data.object.scores.length + (PAGE - 1) * 25
+          }
+          rowsPerPage={rowsPerPage}
+          page={PAGE - 1}
+          onPageChange={handleChangePage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+        ></TablePagination>
+      )}
+
       {error && (
         <Snackbar
           anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
